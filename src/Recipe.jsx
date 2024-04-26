42df17ef6e5bd28e459d193bd11866b1b6073f8f
Yamsmdj <yamine.mdj@gmail.com>
2024-04-26 14:22:43 +0200

    26/04 14:22

---
+++ src/Recipe.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Recipe = () => {
+    return (
+        <div>
+            Recette
+        </div>
+    );
+};
+
+export default Recipe;
--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,12 @@ import App from './App.jsx'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Router>
     <App />
+    </Router>
   </React.StrictMode>,
 )
